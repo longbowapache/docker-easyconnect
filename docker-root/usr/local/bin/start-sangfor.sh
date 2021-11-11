45756df7577e976386d1cc66e1bd205f339fc7df
Qize Xu <qizexu@yangqianguan.com>
2021-11-11 23:33:22 +0800

    add flock

--- docker-root/usr/local/bin/start-sangfor.sh
+++ docker-root/usr/local/bin/start-sangfor.sh
@@ -7,8 +7,11 @@ do
 		# 在 EasyConnect 前端启动过程中，会出现 cms client connect failed 的报错，此时应该启动 sslservice.sh。但这个脚本启动得太早也会没有作用……
 		# 参考了 https://blog.51cto.com/13226459/2476193 ，在此对作者表示感谢。
 		{
-			tail -n 0 -f /usr/share/sangfor/EasyConnect/resources/logs/ECAgent.log | grep "\\[Register\\]cms client connect failed" -m 1
-			/usr/share/sangfor/EasyConnect/resources/shell/sslservice.sh &
+      LOCK_FILE="/var/lock/ecmon.lock"
+      if flock -xn $LOCK_FILE; then
+        tail -n 0 -f /usr/share/sangfor/EasyConnect/resources/logs/ECAgent.log | grep "\\[Register\\]cms client connect failed" -m 1
+        /usr/share/sangfor/EasyConnect/resources/shell/sslservice.sh
+      fi
 		} &
 
 		# 下面这行代码启动 EasyConnect 的前端。
